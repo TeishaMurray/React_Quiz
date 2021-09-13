e824035c197768d6a1ff8aff200725d71bdbfeb3
TeishaMurray <87395500+TeishaMurray@users.noreply.github.com>
2021-09-13 14:10:57 -0400

    style: css styling complete

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../App.css'
 
 const QuizCard = (props) => {
     // console.log("QuizCard line5", props.question3)
@@ -8,10 +9,12 @@ const QuizCard = (props) => {
     return (
         <div>
             <div className="quiz-card">
-                <h2>JS Array Methods Quiz</h2>
-                <h3>{props.quiz.question}</h3>
+                <h2 className="title">JS Array Methods Quiz</h2>
+                <div className="question-block">
+                    <h3>{props.quiz.question}</h3>
+                </div>
                 <div className="answer-btns">
-                    {props.quiz.answers.map((answerOptions) => <button onClick={() => props.nextQuestion(answerOptions.isCorrect)}>{answerOptions.answr}</button>)}
+                    {props.quiz.answers.map((answerOptions) => <button className="buttons" onClick={() => props.nextQuestion(answerOptions.isCorrect)}>{answerOptions.answr}</button>)}
                 </div>
             </div>
         </div>
--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -3,9 +3,9 @@ const ScoreCard = (props) => {
 console.log("ScoreCard line3", "Current Score", props.scoreDisplay)
     return(
         <div>
-            <div>
+            <div className="score-card">
                 <h2>Your score</h2>
-                <div>{props.scoreDisplay}/10</div>
+                <div><span className="actual-score">{props.scoreDisplay}</span><span className="total-score">/10</span></div>
             </div>
         </div>
         
